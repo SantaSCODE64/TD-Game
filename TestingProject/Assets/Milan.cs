4b4058cf2431ebb500c5454977d378cfb79cfd2c
SantaSCODE64 <mikulasmik28@gmail.com>
2023-06-27 15:03:49 +0200

    Milan.cs

---
+++ TestingProject/Assets/Milan.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Milan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Nazdar Milan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
